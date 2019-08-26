d7b08e9f77036c5a6e3ae56906e5d6b5d552f285
HamzaJirah <hamzajirah@gmail.com>
2019-08-26 14:05:47 +0100

    make call to random jokes api

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 // get joke function 
 const getJokes = (e) => {
+  const userInput = document.querySelector('#number').value;
+  
+  // instantiate xhr object 
+  const xmlHttp = new XMLHttpRequest()
 
+  // make call to external api 
+  xmlHttp.open('GET', `http://api.icndb.com/jokes/random/${userInput}`, true);
 
+  e.preventDefault();
 }
 
 // add event listener to  button element
